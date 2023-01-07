3cf822738459acbd8a4dd20b99e38d2c0f9fb7cf
d0nut <nathanial.lattimer@gmail.com>
2023-01-07 01:29:49 -0800

    add documentation

--- src/cli.rs
+++ src/cli.rs
@@ -2,6 +2,7 @@ use clap::{Parser, Subcommand};
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
+/// Backup your Pinboard archives
 pub struct Cli {
     #[command(subcommand)]
     pub subcommand: SubCommand
@@ -9,28 +10,37 @@ pub struct Cli {
 
 #[derive(Subcommand, Debug)]
 pub enum SubCommand {
+    /// Save your Pinboard metadata
     Metadata(MetadataFlags),
+    /// Archive your cached Pinboard archives
     Archive(ArchiveFlags)
 }
 
 #[derive(Parser, Debug)]
 pub struct MetadataFlags {
     #[arg(short, long)]
+    /// Pinboard username.
     pub username: String,
     #[arg(short, long)]
+    /// Pinboard password.
     pub password: String,
     #[arg(short, long, default_value = "pinboard.json")]
+    /// File to write your Pinboard metadata to.
     pub output: String
 }
 
 #[derive(Parser, Debug)]
 pub struct ArchiveFlags {
     #[arg(short, long)]
+    /// Pinboard username.
     pub username: String,
     #[arg(short, long)]
+    /// Pinboard password.
     pub password: String,
     #[arg(short, long, default_value = "pinboard")]
+    /// Directory to save your cached Pinboard archive data to.
     pub output_directory: String,
     #[arg(short, long, default_value = "32")]
+    /// Number of concurrent downloads to run at a time.
     pub concurrency: usize,
 }
